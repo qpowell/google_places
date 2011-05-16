ef1e379432e3b94f5e1e76bd7112e13e0eadbfa2
Marcel de Graaf <mail@marceldegraaf.net>
2011-05-16 22:40:53 +0200

    I should test my changes before pushing to Github

--- lib/google_places.rb
+++ lib/google_places.rb
@@ -2,6 +2,6 @@ require 'rubygems'
 require 'httparty'
 
 
-%w(version client location request spot).each do |file|
+%w(client location request spot).each do |file|
   require File.join(File.dirname(__FILE__), 'google_places', file)
 end
