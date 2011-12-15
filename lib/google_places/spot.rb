c998e76fad04c38bf531724e3b0b9502118b73fc
Andrew Glover <andrew@entitylab.com>
2011-12-15 05:44:09 -0700

    Extra properties for Single Spot request

--- lib/google_places/spot.rb
+++ lib/google_places/spot.rb
@@ -1,6 +1,6 @@
 module GooglePlaces
   class Spot
-    attr_accessor :lat, :lng, :name, :icon, :reference, :vicinity, :types, :id, :formatted_phone_number, :formatted_address, :address_components, :rating, :url, :cid
+    attr_accessor :lat, :lng, :name, :icon, :reference, :vicinity, :types, :id, :formatted_phone_number, :international_phone_number, :formatted_address, :address_components, :street_number, :street, :city, :region, :postal_code, :country, :rating, :url, :cid, :website
 
     def self.list(lat, lng, api_key, options = {})
       radius = options.delete(:radius) || 200
@@ -55,20 +55,38 @@ module GooglePlaces
     end
 
     def initialize(json_result_object)
-      @reference              = json_result_object['reference']
-      @vicinity               = json_result_object['vicinity']
-      @lat                    = json_result_object['geometry']['location']['lat']
-      @lng                    = json_result_object['geometry']['location']['lng']
-      @name                   = json_result_object['name']
-      @icon                   = json_result_object['icon']
-      @types                  = json_result_object['types']
-      @id                     = json_result_object['id']
-      @formatted_phone_number = json_result_object['formatted_phone_number']
-      @formatted_address      = json_result_object['formatted_address']
-      @address_components     = json_result_object['address_components']
-      @rating                 = json_result_object['rating']
-      @url                    = json_result_object['url']
-      @cid                    = json_result_object['url'].to_i
+      @reference                  = json_result_object['reference']
+      @vicinity                   = json_result_object['vicinity']
+      @lat                        = json_result_object['geometry']['location']['lat']
+      @lng                        = json_result_object['geometry']['location']['lng']
+      @name                       = json_result_object['name']
+      @icon                       = json_result_object['icon']
+      @types                      = json_result_object['types']
+      @id                         = json_result_object['id']
+      @formatted_phone_number     = json_result_object['formatted_phone_number']
+      @international_phone_number = json_result_object['international_phone_number']
+      @formatted_address          = json_result_object['formatted_address']
+      @address_components         = json_result_object['address_components']
+      @street_number              = address_component(:street_number, 'short_name')
+      @street                     = address_component(:route, 'long_name')
+      @city                       = address_component(:locality, 'long_name')
+      @region                     = address_component(:administrative_area_level_1, 'long_name')
+      @postal_code                = address_component(:postal_code, 'long_name')
+      @country                    = address_component(:country, 'long_name')
+      @rating                     = json_result_object['rating']
+      @url                        = json_result_object['url']
+      @cid                        = json_result_object['url'].to_i
+      @website                    = json_result_object['website']
+    end
+
+    def address_component(address_component_type, address_component_length)
+      if component = address_components_of_type(address_component_type)
+        component.first[address_component_length] unless component.first.nil?
+      end
+    end
+    
+    def address_components_of_type(type)
+      @address_components.select{ |c| c['types'].include?(type.to_s) } unless @address_components.nil?
     end
 
   end
