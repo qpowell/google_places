004af0bb4afa4ce6de2207e59c71f3145a739666
Michael Wagner <mitch.wagna@gmail.com>
2012-10-24 13:07:31 +0200

    adding api docs to errors

--- lib/google_places/error.rb
+++ lib/google_places/error.rb
@@ -2,9 +2,21 @@ module GooglePlaces
   class OverQueryLimitError < HTTParty::ResponseError
   end
 
+  # Thrown when a request was denied by the server
+  #
+  # This can be the case when:
+  # - querying the SPOT_LIST_URL <b>without</b> the following parameters:
+  # - - key
+  # - - sensor
   class RequestDeniedError < HTTParty::ResponseError
   end
 
+  # Thrown when a request was rejected as invalid by the server
+  #
+  # This can be the case when:
+  # - querying the SPOT_LIST_URL <b>without</b> the following parameters:
+  # - - location
+  # - - radius
   class InvalidRequestError < HTTParty::ResponseError
   end
 
