fff0439be24bae3d62184f4eaafbf559e3170eae
Marcel de Graaf <mail@marceldegraaf.net>
2012-12-03 15:35:55 +0100

    Fix failing spec

--- spec/google_places/location_spec.rb
+++ spec/google_places/location_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe GooglePlaces::Location do
 
   it 'should return a location based on a lat and lng' do
-    GooglePlaces::Location.new('123', '456').format.should == '123,456'
+    GooglePlaces::Location.new('123', '456').format.should == '123.00000000,456.00000000'
   end
 
 end
