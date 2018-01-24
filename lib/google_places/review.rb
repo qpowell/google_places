b36ecd20daa750eed5dfae41e17c7c539c277dcb
Edwin Wills <edwin@rentify.com>
2018-01-24 10:12:00 +0000

    Include the profile_photo_url attribute for reviews

--- lib/google_places/review.rb
+++ lib/google_places/review.rb
@@ -1,14 +1,15 @@
 module GooglePlaces
   class Review
-    attr_accessor :rating, :type, :author_name, :author_url, :text, :time
+    attr_accessor :rating, :type, :author_name, :author_url, :text, :time, :profile_photo_url
 
-    def initialize(rating, type, author_name, author_url, text, time)
-      @rating       = rating
-      @type         = type
-      @author_name  = author_name
-      @author_url   = author_url
-      @text         = text
-      @time         = time
+    def initialize(rating, type, author_name, author_url, text, time, profile_photo_url)
+      @rating            = rating
+      @type              = type
+      @author_name       = author_name
+      @author_url        = author_url
+      @text              = text
+      @time              = time
+      @profile_photo_url = profile_photo_url
     end
   end
 end
