03bbbdc26a961fff1782a10e25abc626e9057be8
Edwin Wills <edwin@rentify.com>
2019-06-18 11:52:21 +0100

    Clean up VCR cassettes

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -4,7 +4,7 @@ VCR.configure do |c|
   c.cassette_library_dir     = 'spec/vcr_cassettes'
   c.hook_into                  :webmock
   c.ignore_localhost         = true
-  c.default_cassette_options = { :record => :new_episodes }
+  c.default_cassette_options = { :record => :none }
   c.configure_rspec_metadata!
   c.filter_sensitive_data("<API_KEY>") { RSPEC_API_KEY }
 end
