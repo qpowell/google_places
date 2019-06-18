856f4026561439959f95e253ca207220b007c7be
Edwin Wills <edwin@rentify.com>
2019-06-18 11:42:43 +0100

    Add API key filter

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -6,4 +6,5 @@ VCR.configure do |c|
   c.ignore_localhost         = true
   c.default_cassette_options = { :record => :new_episodes }
   c.configure_rspec_metadata!
+  c.filter_sensitive_data("<API_KEY>") { RSPEC_API_KEY }
 end
